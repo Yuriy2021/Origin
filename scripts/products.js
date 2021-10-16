01c99275d490a62b1d7e7ec2a318d23faafaec3b
Yuriy <xstream_06@mail.ru>
2021-10-16 19:37:47 +0300

    Lesson8JS

---
+++ scripts/products.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const products = [
+    new ProductDTO(
+        0,
+        'img/item-1.png',
+        'Product 0',
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+        52.00,
+    ),
+    new ProductDTO(
+        1,
+        'img/item-7.png',
+        'Product 1',
+        'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+        52.00,
+    ),
+    new ProductDTO(
+        2,
+        'img/item-8.png',
+        'Product 2',
+        'Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.',
+        52.00,
+    ),
+    new ProductDTO(
+        3,
+        'img/item-9.png',
+        'Product 3',
+        'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.',
+        52.00,
+    ),
+    new ProductDTO(
+        4,
+        'img/item-10.png',
+        'Product 4',
+        'Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.',
+        52.00,
+    ),
+    new ProductDTO(
+        5,
+        'img/item-11.png',
+        'Product 5',
+        'Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.',
+        52.00,
+    ),
+    new ProductDTO(
+        6,
+        'img/item-12.png',
+        'Product 6',
+        'Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.',
+        52.00,
+    ),
+    new ProductDTO(
+        7,
+        'img/item-13.png',
+        'Product 7',
+        'Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.',
+        52.00,
+    ),
+    new ProductDTO(
+        8,
+        'img/item-14.png',
+        'Product 8',
+        'Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.',
+        52.00,
+    ),
+];
