8a0e157a119bb99875202e41aeaf29e4c7d0c950
Yuriy <xstream_06@mail.ru>
2021-11-25 17:41:00 +0300

    JS_adv_Lesson2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const goods = [
+/* const goods = [
     { title: 'Shirt', price: 150 },
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
@@ -21,6 +21,63 @@ const renderGoodsList = (list = goods) => {
     $goodsList.insertAdjacentHTML('beforeend', goodsList);
 }
 
-renderGoodsList();
+renderGoodsList(); */
 
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
 
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+}
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+
+        ];
+    }
+
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+    calculateGoodList() {
+        let sum = 0
+        this.goods.forEach(element => sum += element.price)
+        alert("Сумма товаров = " + sum)
+    }
+
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.calculateGoodList();
+
+class Basketlist {
+    //addToBasket()-добавление элементов в корзину
+    //baskeRender() - отображение корзины
+    //basketcalculate() - подсчет суммы корзины
+
+}
+
+class BasketItem {
+
+    //basketItemRender() - отрисовка элементов в корзине
+}
