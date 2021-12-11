d2021bbe2e562a4a1e82650d61bd879d84fbd963
Yuriy <xstream_06@mail.ru>
2021-12-11 23:18:00 +0300

    JS_adv_Lesson5

--- regexp_script.js
+++
@@ -1,6 +0,0 @@
-// const str = 'We exclaimed, "It\'s so easy!"';
-// const regexp = /\"/g;
-// console.log(str.replace(regexp, '\''));
-const str = "We exclaimed, '\It\'s so easy!\'";
-const regexp = /\'(?!s)/g;
-console.log(str.replace(regexp, '\"'));
